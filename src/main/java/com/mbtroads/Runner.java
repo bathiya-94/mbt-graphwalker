ef40a8cd67aaa531ad96b312ff7a5456c26f0273
Bathiya Thennakoon <bathiya-94>
2021-06-29 16:28:13 +0530

    Added GraphWalker Player

--- src/main/java/com/mbtroads/Runner.java
+++ src/main/java/com/mbtroads/Runner.java
@@ -1,21 +1,28 @@
 package com.mbtroads;
 
-import com.mbtroads.SomeSmallTest;
 import org.graphwalker.java.test.Result;
 import org.graphwalker.java.test.TestExecutor;
+import org.graphwalker.websocket.WebSocketServer;
 
 import java.io.IOException;
 
 public class Runner {
   public static void main(String[] args) throws IOException {
-//    TestExecutor executor = new TestExecutor(
-//      SomeSmallTest.class
-//    );
 
     TestExecutor executor1 = new TestExecutor(ArticleCreationTest.class);
 
 //    Result result = executor.execute(true);
+//    Result result1 = executor1.execute(true);
+
+    WebSocketServer server = new WebSocketServer(8887, executor1.getMachine());
+    server.start();
+
     Result result1 = executor1.execute(true);
+    if (result1.hasErrors()) {
+      for (String error : result1.getErrors()) {
+        System.out.println(error);
+      }
+    }
 
 //    System.out.println("Done: [" + result.getResults().toString(2) + "]\n");
     System.out.println("Done: [" + result1.getResults().toString(2) + "]\n");
